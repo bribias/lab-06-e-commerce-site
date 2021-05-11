45da110532f6ded63c08259dcc1ae378e70a25f2
bribias <brianna.bias@gmail.com>
2021-05-11 15:29:07 -0700

    CSS update to cart

--- cart/renderCart.js
+++ cart/renderCart.js
@@ -1,1 +1,58 @@
-import plants from '../functionality/plants';
+// we know we will need our raw animal data because that's where the name and price live
+import { plants } from '../functionality/plants.js';
+import { cartItems } from '../cart-items.js';
+// we know we will need findById because that's where the quantity lives
+import { findById } from '../utils.js';
+// plants has price, name, etc
+// cartItem has id, quantity
+// this function should take in a cart item and return a table row with all the good data
+export function renderCartItem(cartItem) {
+    // cart item has only: quantity and id
+    // we need price and the name of the item
+    // the price and the name live in the rows of the other array
+    // to get the name and the species, we need to get the id from the cartItem and go find the corresponding animal in the feltAnimals array. luckily we have a function that can do just that!
+    const plants = findById(plants, cartItem.id);
+
+    // then all we have to do is make some DOM elements
+    const tr = document.createElement('tr');
+    const nameTd = document.createElement('td');
+    const quantityTd = document.createElement('td');
+    const priceTd = document.createElement('td');
+    const totalTd = document.createElement('td');
+
+    // and inject the species from the plants
+    nameTd.textContent = plants.species;
+    // the quantity from the cart item
+    quantityTd.textContent = cartItem.quantity;
+    // and the price from the felt animal
+    priceTd.textContent = plants.price.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
+    const total = plants.price * cartItem.quantity;
+
+    totalTd.textContent = total.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+    // append those to our tr
+    tr.append(nameTd, quantityTd, priceTd, totalTd);
+
+    // and return that tr
+    return tr;
+}
+
+export function getCartTotal() {
+    let accumulator = 0;
+
+    for (let item of cartItems) {
+        const plants = findById(plantss, item.id);
+
+        const total = item.quantity * plants.price;
+
+        accumulator = accumulator + total;
+    }
+
+    return accumulator;
+}
