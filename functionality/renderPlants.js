fb62b37d4f64d2509817d68737478c532bab5cca
bribias <brianna.bias@gmail.com>
2021-05-12 14:56:32 -0700

    local storage and CSS changes

--- functionality/renderPlants.js
+++ functionality/renderPlants.js
@@ -6,17 +6,18 @@ export function renderPlants(plants) {
     outerDiv.classList.add('product');
     
     const li = document.createElement('li');
-    li.className = plants.category;
-    li.title = plants.description;
+    li.className = plants.species;
+
+    const pDescript = document.createElement('p');
+    pDescript.textContent = plants.description;
 
     const h3 = document.createElement('h3');
-    h3.textContent = plants.name;
-    li.appendChild(h3);
+    h3.textContent = plants.species;
 
     const img = document.createElement('img');
     img.src = './images/' + plants.image;
     img.alt = plants.name + ' image';
-    li.appendChild(img);
+    console.log(li);
 
     const p = document.createElement('p');
     p.className = 'price';
@@ -36,6 +37,7 @@ export function renderPlants(plants) {
         li,
         h3,
         img,
+        pDescript,
         p,
         button
     );
